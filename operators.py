4810e54329fa68ec778ee22c9959a4dc985940f6
22hr1a3047 <22hr1a3047@gmail.com>
2024-09-18 21:14:28 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -18,3 +18,128 @@ cAge=int(input())
 deviAge =((cAge+3)*4)-3
 print(deviAge)
 
+'''
+2) Dhoni once wished to join a a reputed Cricket Coaching Camp to be held at a place "X" kms away from his house. He told about this to his father and got his consent to use his friend's bike for the Camp. The Camp was to be held on all days of the month. His friend's bike provides a mileage of Y km/litre and the cost of petrol was Rs. Z. Dhoni's father now wanted to know the total amount that was needed by Dhoni to spend on his travel to the Camp. Help him find the same and assume number of days in a month as 30 days.
+Input Format:
+First line of the input is an integer "X" in kms that specifies the distance of the Camp from Dhoni's house.
+Second line is an integer "Y" in km/litre that specifies the mileage of his friend's bike.
+Third line is a float "Z" that specifies the cost of petrol in rupees.
+Output Format:
+Output should display a float that gives the total amount that is needed by Dhoni to spend on his travel in rupees. The float value is displayed correct to 2 decimal places.
+Sample Input and Output 1:
+75
+55
+63
+2577.27
+Sample Input and Output 2:
+35
+78
+65.0
+875.00
+'''
+#code for second question
+
+x=float(input())
+y=float(input())
+z=float(input())
+a=(x*z*30)/y
+print(round(a,2))
+
+'''
+3)MS Dhoni is a grade A player according to BCCI Central Contracts in 2016. MSD's net worth in 2016 is around 31 million and is said to be the richest Indian cricketer.
+Apart from his salary as an Indian cricketer, Dhoni endorses various popular brands and earns a large amount from endorsements. Besides that individual and team bonuses are also given on the basis of individual and team performances. So precisely the sources of income for Dhoni are from - Salary, Bonuses and Awards through Winning and Endorsements.
+Write a program that finds Dhoni's percentage of income earned from each of the 3 individual sources.
+Input Format:
+First line of the input is an integer that specifies Dhoni's income in rupees from Salary.
+Second line is an integer that specifies Dhoni's income in rupees from Bonuses and Awards.
+Third line is an integer that specifies Dhoni's income in rupees from endorsements.
+Output Format:
+Output should display 3 floats in a line, separated by a space. The first float corresponds to the percentage of income from Salary, the second float corresponds to the percentage of income from Bonuses and Awards and the third float corresponds to the percentage of income from endorsements.
+All float values should be displayed correct to 2 decimal places.
+Sample Input and Output 1:
+100
+20
+80
+50.00 10.00 40.00
+Sample Input and Output2:
+50000
+10000
+35000
+52.63 10.53 36.84
+Note: Bold highlighted is the output
+'''
+#code for third question
+s=int(input ())
+b=int(input ())
+e=int(input ())
+ti=s+b+e
+ps=(s/ti)*100
+pb=(b/ti)*100
+pe=(e/ti)*100
+print("%.2f"%ps,"%.2f"%pb,"%.2f"%pe)
+
+'''
+4)
+Extras
+Extras are runs scored by a means other than a batsman hitting the ball. A batsman is not given credit for extras other than runs scored off the bat from a no ball, and the extras are tallied separately on the scorecard and count only towards the team’s score. the types of extras are No ball, Wide, Bye, Leg-bye and Penalty. 1 Penalty corresponds to 5 runs.
+Find the total runs that the Extras contribute to the team’s score, given the number of No-balls, wides, byes, leg-byes and penalty given off by the bowlers in innings.
+Input format:
+First line of the input contains an integer that corresponds to the number of No-balls.
+Second line of the input contain an integer that corresponds to the numbers of wides.
+Third line of the input contains an integer that corresponds to the number of byes.
+Fourth line of the input contain an integer that corresponds to the numbers of led-byes.
+Fifth line of the input contains an integer that corresponds to the numbers of penalty runs.
+Output format:
+Output should display an integer that returns the total runs that the extras together contribute to team’s total.
+Sample input and output 1:
+4
+7
+3
+10
+3
+39
+Sample input and output 2:
+2
+3
+7
+1
+17
+'''
+
+#code for fourth question
+a=int(input("number of no balls"))
+b=int(input("number of wides"))
+c=int(input("number of no byes"))
+d=int(input("number of no led-byes"))
+e=int(input("number of penalty runs"))
+total=a+b+c+d+e*5
+print(total)
+
+'''
+5)
+Tamilnadu was battling one if it’s worst floods in a century last December, as several part of the state have been submerged and cut off from essential supplies. It was heartening that the Cricketers came forward to contribute for the cause of floods and it was decided amongst the team that the senior players donate 50% of their salary and junior players to donate 40% against the flood relief measures
+Assume there are 6 senior players and 5 junior players. The salary of senior players Rs.X and that of junior players is Rs.Y. Find the total contribution from the cricket team towards the floods.
+Input format:
+First line of the input is an integer “X” that specifies the salary of the senior players in rupees.
+Second line is an integer “Y” that specifies the salary of the junior players in rupees.
+Output format:
+Output should display a flood that gives the total contribution of money in rupees from the cricket team. The float value should be displayed correct to 2 decimal places.
+Sample input and output 1:
+45000
+40000
+215000.00
+Sample input and output 2:
+78000
+60000
+354000.00
+'''
+#code for fifth problem
+X = int(input("the salary of senior players : "))
+Y = int(input("the salary of junior players : "))
+senior_players = 6
+junior_players = 5
+senior_contribution = senior_players * (0.50 * X)
+junior_contribution = junior_players * (0.40 * Y)
+total_contribution = senior_contribution + junior_contribution
+print(f"Total contribution towards flood relief: Rs {total_contribution:.2f}")
+
